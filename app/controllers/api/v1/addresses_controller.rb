6b416d627230afa2b6ac57f6d6f18d3848c26f03
Thiago Sawada <tms1_tms2@hotmail.com>
2023-09-30 22:00:57 -0300

    refactor: AddressesController

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -2,52 +2,55 @@ class Api::V1::AddressesController < ApplicationController
   before_action :authenticate_api_v1_user!
   before_action :clean_cep
   before_action :set_user
+  around_action :handle_errors
 
   def create
-    begin
-      fetch_service = FetchAddressService.new(@cep, @user)
-      fetch_result = fetch_service.call
+    fetch_service = FetchAddressService.new(@cep, @user)
+    serialized_address = SerializeAddressService.new(fetch_service.call).serialize
+    create_service = CreateAddressService.new(@user, serialized_address)
 
-      serialized_address = SerializeAddressService.new(fetch_result).serialize
+    @address = create_service.save
 
-      create_service = CreateAddressService.new(@user, serialized_address)
-      @address = create_service.save
-
-      render :create, status: :created
-    rescue CepExceptions::InvalidCepFormat
-      render json: { status: 400, error: I18n.t("errors.invalid_cep_format") }, status: :bad_request
-    rescue CepExceptions::CepNotFound
-      render json: { status: 404, error: I18n.t("errors.cep_not_found") }, status: :not_found
-    rescue CepExceptions::ServiceError
-      render json: { status: 503, error: I18n.t("errors.service_error") }, status: :service_unavailable
-    rescue CepExceptions::TimeoutError
-      render json: { status: 408, error: I18n.t("errors.timeout_error") }, status: :request_timeout
-    rescue ActiveRecord::RecordNotFound
-      render json: { status: 404, error: I18n.t("errors.user_not_found") }, status: :not_found
-    rescue CepExceptions::CepAlreadyAssociatedError => e
-      @associated_address = e.address
-      render :already_associated_error, status: :unprocessable_entity
-    rescue CepExceptions::AddressValidationError => e
-      render json: {
-        status: 422, error: I18n.t("errors.address_validation_error", errors: e.message)
-      }, status: :unprocessable_entity
-    rescue StandardError => e
-      logger.error "Unhandled error: #{e}"
-      render json: { status: 500, error: I18n.t("errors.something_went_wrong") }, status: :internal_server_error
-    end
+    render :create, status: :created
   end
 
   private
 
-    def clean_cep
-      @cep ||= address_params[:cep].to_s.gsub(/\D/, "")
-    end
+  def clean_cep
+    @cep ||= address_params[:cep].to_s.gsub(/\D/, "")
+  end
 
-    def address_params
-      params.require(:address).permit(:cep)
-    end
+  def address_params
+    params.require(:address).permit(:cep)
+  end
 
-    def set_user
-      @user ||= User.find(params[:user_id])
-    end
+  def set_user
+    @user ||= User.find(params[:user_id])
+  end
+
+  def handle_errors
+    yield
+  rescue CepExceptions::InvalidCepFormat
+    render_error(:bad_request, "errors.invalid_cep_format")
+  rescue CepExceptions::CepNotFound
+    render_error(:not_found, "errors.cep_not_found")
+  rescue CepExceptions::ServiceError
+    render_error(:service_unavailable, "errors.service_error")
+  rescue CepExceptions::TimeoutError
+    render_error(:request_timeout, "errors.timeout_error")
+  rescue ActiveRecord::RecordNotFound
+    render_error(:not_found, "errors.user_not_found")
+  rescue CepExceptions::CepAlreadyAssociatedError => e
+    @associated_address = e.address
+    render :already_associated_error, status: :unprocessable_entity
+  rescue CepExceptions::AddressValidationError => e
+    render_error(:unprocessable_entity, "errors.address_validation_error", errors: e.message)
+  rescue StandardError => e
+    logger.error "Unhandled error: #{e}"
+    render_error(:internal_server_error, "errors.something_went_wrong")
+  end
+
+  def render_error(status, message_key, extras = {})
+    render json: { status: Rack::Utils::SYMBOL_TO_STATUS_CODE[status], error: I18n.t(message_key, extras) }, status: status
+  end
 end
